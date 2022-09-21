6d797aab1b1c7bd934ed0c5092c0879e5777fe70
leahmarv <lmarville@smith.edu>
2022-09-20 22:06:24 -0400

    Added attributes and constructors, transferred methods

--- computer.py
+++ computer.py
@@ -2,9 +2,41 @@ class Computer:
 
     # What attributes will it need?
 
+    from typing import Dict, Union
+
+    description: str = ""
+    processor_type: str = ""
+    hard_drive_capacity: int = 0
+    memory: int = 0
+    operating_system: str = ""
+    year_made: int = 0
+    price: int = 0
+
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self, description: str, processor_type: str, hard_drive_capacity: int, memory: int, operating_system: str, year_made: int, price: int):
+        self.__description = description
+        self.__processor_type = processor_type
+        self.__hard_drive_capacity = hard_drive_capacity
+        self.__memory = memory
+        self.__operating_system = operating_system
+        self.__year_made = year_made
+        self.__price = price
 
     # What methods will you need?
+
+    def create_computer(description: str,
+                    processor_type: str,
+                    hard_drive_capacity: int,
+                    memory: int,
+                    operating_system: str,
+                    year_made: int,
+                    price: int):
+        return {'description': description,
+            'processor_type': processor_type,
+            'hard_drive_capacity': hard_drive_capacity,
+            'memory': memory,
+            'operating_system': operating_system,
+            'year_made': year_made,
+            'price': price
+    }
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,61 @@
 class ResaleShop:
 
-    # What attributes will it need?
+    # What attributes will it need? (inventory and itemID)
+
+    from typing import Dict, Union, Optional
+    inventory: Dict[int, Dict[str, Union[str, int, bool]]] = {}
+    itemID: int = 0
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self, inventory: dict, itemID: int):
+        self.__inventory = inventory
+        self.__itemID = itemID
 
     # What methods will you need?
+    def buy(computer: Dict[str, Union[str, int, bool]]):
+        global itemID
+        global inventory
+        itemID += 1 # increment itemID
+        inventory[itemID] = computer
+        return itemID
+
+    def update_price(item_id: int, new_price: int):
+        if item_id in inventory:
+            inventory[item_id]["price"] = new_price
+        else:
+            print("Item", item_id, "not found. Cannot update price.")
+
+    def sell(item_id: int):
+        if item_id in inventory:
+            del inventory[item_id]
+            print("Item", item_id, "sold!")
+        else: 
+            print("Item", item_id, "not found. Please select another item to sell.")
+    
+    def print_inventory():
+    # If the inventory is not empty
+        if inventory:
+        # For each item
+            for item_id in inventory:
+            # Print its details
+                print(f'Item ID: {item_id} : {inventory[item_id]}')
+        else:
+            print("No inventory to display.")
+
+    def refurbish(item_id: int, new_os: Optional[str] = None):
+        if item_id in inventory:
+            computer = inventory[item_id] # locate the computer
+            if int(computer["year_made"]) < 2000:
+                computer["price"] = 0 # too old to sell, donation only
+            elif int(computer["year_made"]) < 2012:
+                computer["price"] = 250 # heavily-discounted price on machines 10+ years old
+            elif int(computer["year_made"]) < 2018:
+                computer["price"] = 550 # discounted price on machines 4-to-10 year old machines
+            else:
+                computer["price"] = 1000 # recent stuff
+
+            if new_os is not None:
+                computer["operating_system"] = new_os # update details after installing new OS
+        else:
+            print("Item", item_id, "not found. Please select another item to refurbish.")
